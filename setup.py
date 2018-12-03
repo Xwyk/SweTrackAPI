1da3662e532e9c1f0bb1f1471e497d9146bd4b50
Øystein Hansen <oeysteinhansen@gmail.com>
2018-12-03 17:19:05 +0100

    Adding initial setup files.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyswetrack',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='API for SweTrack Live Tracking cloud platform. swetrack.com',
+    long_description=open('readme.md').read(),
+    install_requires=['requests'],
+    url='https://github.com/oeysteinhansen/pyswetrack',
+    author='Øystein Hansen',
+    author_email='oeysteinhansen@gmail.com'
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
